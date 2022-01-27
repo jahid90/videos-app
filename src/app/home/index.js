1a94341d83e05a87991f03ae084a6a320d31bc91
Jahid <jahid90@users.noreply.github.com>
2022-01-27 14:04:59 +0530

    add prettier config

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -2,37 +2,37 @@ const camelCaseKeys = require('camelcase-keys');
 const express = require('express');
 
 const createHandlers = ({ queries }) => {
-
     const home = (req, res, next) => {
         return queries
             .loadHomePage()
-            .then(viewData => res.render('home/templates/home', viewData.pageData))
+            .then((viewData) =>
+                res.render('home/templates/home', viewData.pageData)
+            )
             .catch(next);
-    }
+    };
 
     return {
         home,
-    }
-}
+    };
+};
 
 const createQueries = ({ db }) => {
-
     const loadHomePage = () => {
-        return db.then(client => client('pages')
-            .where({ page_name: 'home' })
-            .limit(1)
-            .then(camelCaseKeys)
-            .then(rows => rows[0])
+        return db.then((client) =>
+            client('pages')
+                .where({ page_name: 'home' })
+                .limit(1)
+                .then(camelCaseKeys)
+                .then((rows) => rows[0])
         );
-    }
+    };
 
     return {
         loadHomePage,
-    }
-}
+    };
+};
 
 const createHome = ({ db }) => {
-
     const queries = createQueries({ db });
     const handlers = createHandlers({ queries });
 
@@ -43,7 +43,7 @@ const createHome = ({ db }) => {
         handlers,
         queries,
         router,
-    }
-}
+    };
+};
 
 module.exports = createHome;
